05de196229560943a526777ffffc9f96102ea7ea
shri kamane <shrirudra008@gmail.com>
2026-06-09 17:14:03 +0530

    new file added

---
+++ FunctionDemo.py
@@ -0,0 +1,14 @@
+
+def calculate_area(width,height):
+    return width*height
+
+h = 10
+w = 3
+Area = calculate_area(h,w)
+print("Area is :",Area)
+
+def perimeter (w,h):
+    return 2*(w+h)
+
+perimeter = perimeter(2,3)
+print("Perimeter : ",perimeter)
