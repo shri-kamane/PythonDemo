7b7ab24b359a0708eeb459be52c77086bd0ab24a
shri kamane <shrirudra008@gmail.com>
2026-06-12 12:45:27 +0530

    some changes

--- FunctionDemo.py
+++ FunctionDemo.py
@@ -1,14 +1,14 @@
+def perimeter (w,h):
+    return 2*(w+h)
 
 def calculate_area(width,height):
     return width*height
 
 h = 10
 w = 3
+
 Area = calculate_area(h,w)
 print("Area is :",Area)
 
-def perimeter (w,h):
-    return 2*(w+h)
-
-perimeter = perimeter(2,3)
-print("Perimeter : ",perimeter)
+perimeter = perimeter(w,h)
+print("Perimeter is :",perimeter)
