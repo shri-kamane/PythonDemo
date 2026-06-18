f5b039e2fb384d35d607932aa3b838e7b28b8079
shri kamane <shrirudra008@gmail.com>
2026-06-18 09:56:00 +0530

    Revert "removed all file"
    This reverts commit 05561703c012713e694a12144014acdf436d39bf.

---
+++ Assignment_No_1.py
@@ -0,0 +1,15 @@
+
+name = input("Enter your name: ")
+age = int (input("Enter Your age in Infocard: "))
+city = input("Enter You City In Infocard:")
+fav_sub = input("Enter You Favorite Subject In Infocard: ")
+
+print(" ")
+
+print("-----------Infocard------------")
+print("     Name : ",name)                
+print("     Age  : ",age)
+print("     City :",city)
+print("   Favorite Subject :",fav_sub)
+print("-------------------------------")
+
---
+++ FunctionDemo.py
@@ -0,0 +1,14 @@
+def perimeter (w,h):
+    return 2*(w+h)
+
+def calculate_area(width,height):
+    return width*height
+
+h = 10
+w = 3
+
+Area = calculate_area(h,w)
+print("Area is :",Area)
+
+perimeter = perimeter(w,h)
+print("Perimeter is :",perimeter)
